63308f8581847bc0f290cb4610fc37406b157e83
pramodkuarp6 <pramodkumarp6@gmail.com>
2020-10-08 01:49:59 +0530

    done

--- app/src/main/java/com/all/in/one/pramod/navigation/Notifications.java
+++ app/src/main/java/com/all/in/one/pramod/navigation/Notifications.java
@@ -12,9 +12,6 @@ public class Notifications extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
-
-
-
         getSupportActionBar().setTitle("Notification");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
